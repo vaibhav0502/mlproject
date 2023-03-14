3c08f5373b441171397fbed17ff1f657ff083a47
vaibhav0502 <kumbharvaibhav007@gmail.com>
2023-03-14 18:50:43 +0530

    model training and hyperparameter tunning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,9 +9,11 @@ from src.exception import CustomException
 from src.logger import logging
 
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 ARTIFACTS = 'artifacts'
 DATA_PATH = os.path.normpath('notebook/data/stud.csv')
+
 
 @dataclass
 class DataIngestionConfig:
@@ -19,18 +21,20 @@ class DataIngestionConfig:
     test_data_path: str = os.path.join(ARTIFACTS, 'test.csv')
     raw_data_path: str = os.path.join(ARTIFACTS, 'data.csv')
 
+
 class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
-        os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+        os.makedirs(os.path.dirname(
+            self.ingestion_config.train_data_path), exist_ok=True)
 
     def initiate_data_ingestion(self):
         '''
         Function to initiate data
-        
+
         Returns:
                 tuple(str, str): train and test data path
-        
+
         '''
 
         logging.info("Data ingestion started")
@@ -39,27 +43,35 @@ class DataIngestion:
             df = pd.read_csv(DATA_PATH)
             logging.info("Read dataset done.")
 
-            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            df.to_csv(self.ingestion_config.raw_data_path,
+                      index=False, header=True)
             logging.info("Train test split initiated")
 
-            train_set, test_set = train_test_split(df,test_size=0.2,random_state=42)
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            train_set, test_set = train_test_split(
+                df, test_size=0.2, random_state=42)
+            train_set.to_csv(
+                self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,
+                            index=False, header=True)
 
             logging.info("Data ingestion completed")
             return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-                )
+            )
         except Exception as e:
             logging.error(CustomException(e, sys))
             # raise CustomException(e, sys)
             return (None, None)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
     train_arr, test_arr, preprocessor_obj_file_path = data_transformation.initiate_data_transformation(
         train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
 import os
 import sys
-
-import numpy as np 
+import time
+import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
+from src.logger import logging
+
 
 def save_object(file_path, obj):
     try:
@@ -20,24 +22,42 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
+        ss = time.time()
         for i in range(len(list(models))):
+            s = time.time()
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+            logging.info(
+                f"Model training started for: {list(models.keys())[i]}")
 
-            model.fit(X_train, y_train)  # Train model
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            # model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
+
+            e = time.time()
+            logging.info(
+                f"Model training completed for: {list(models.keys())[i]}, time: {e-s} and R2 score, train: {train_model_score}, test: {test_model_score}")
+            logging.info("--"*25)
+        tt = time.time()
+        logging.info(f"Total time: {tt-ss}")
 
         return report
 
